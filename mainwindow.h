99d64c2758f75a4c47597675fdd06fc78d17bcd7
Deniz Garip <84642530+Deniz-Garip@users.noreply.github.com>
2026-06-01 14:46:01 +0300

    Add MainWindow class header for calculator UI

---
+++ mainwindow.h
@@ -0,0 +1,68 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLabel>
+using namespace std;
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() ;
+
+private slots:
+
+    void incriemnat();
+    void decrement();
+    void divide();
+    void equal();
+    void number_zero();
+    void number_one();
+    void number_two();
+    void number_three();
+    void number_four();
+    void number_five();
+    void number_six();
+    void number_seven();
+    void number_eight();
+    void number_nine();
+    void example(int num);
+
+
+private:
+
+    QPushButton * btn_plush;
+    QPushButton * btn_minus;
+    QPushButton * btn_division;
+    QPushButton * btn_times;
+    QPushButton * btn_equal;
+    QPushButton * btn_zero;
+    QPushButton *btn_one;
+    QPushButton *btn_two;
+    QPushButton *btn_three;
+    QPushButton *btn_four;
+    QPushButton *btn_five;
+    QPushButton *btn_six;
+    QPushButton *btn_seven;
+    QPushButton *btn_eight;
+    QPushButton *btn_nine;
+
+    QLabel *label;
+    int number =0;
+    string symbol;
+    int firstnumber =0;
+    int secondnumber =0;
+    bool clicked= false;
+
+};
+#endif // MAINWINDOW_H
